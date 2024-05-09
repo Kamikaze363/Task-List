d4c71f738cec54d191824749320aa3c41270c8cf
Rolan <107186461+Kamikaze363@users.noreply.github.com>
2024-05-09 13:30:46 +0200

    tested routes

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -3,5 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
 });
+
+Route::get('/xxx', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello, ' . $name . '!';
+});
+
+Route::fallback(function(){
+    return 'Page not found';
+});
+
+
+//Get gets data from the server
+//Post sends data to the server
+//Put updates data on the server
+//Delete deletes data on the server
